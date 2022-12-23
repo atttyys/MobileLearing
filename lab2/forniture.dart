ec27e9778fdc8b2b9b237735ad842a07b220d478
Chollathit Saensaradee <huntgame2v2@gmail.com>
2022-12-23 18:50:19 +0700

    success

--- lab2/forniture.dart
+++ lab2/forniture.dart
@@ -1,17 +1,26 @@
 import 'product.dart';
 
-class Forniture extends Product{
+class Forniture extends Product {
   Forniture(super.name, super.price); // new constructor
-  
+
   // state 6 >>
   int _amount = 0;
-  getAmount()=>this._amount;
-  setAmount(int amount)=>this._amount = amount;
+  getAmount() => this._amount;
+  setAmount(int amount) => this._amount = amount;
   // state 6 <<
 
   // overriding method
-  void show2(int i,[String? name]){
-      print('สินค้า ' + ' ราคา ');
-    }
-
+  void show2(int n, double i, [String? name]) {
+    var name2 = name;
+    var i2 = i.toString();
+    var n2 = i * n;
+    print('สินค้า ' +
+        name2! +
+        ' ราคา ' +
+        i2 +
+        ' จำวนวณ ' +
+        n.toString() +
+        ' ชิ้น ราคา ' +
+        n2.toString());
+  }
 }
