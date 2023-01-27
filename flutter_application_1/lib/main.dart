f443a9762357ca07e85283dff218077d5a10dd26
Chollathit Saensaradee <huntgame2v2@gmail.com>
2023-01-27 02:21:18 -0800

    lab3-2

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(const MyApp());
 }
 
@@ -11,44 +11,92 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "Hello",
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'Appbar',
-              style: TextStyle(fontSize: 30, color: Colors.lightGreenAccent),
-            ),
-            backgroundColor: Colors.black87,
-          ),
-          body: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const <Widget>[
-                Text(
-                  "Chollathit Saensaradee",
-                  style: TextStyle(fontSize: 32, color: Colors.black87),
-                ),
-                Image(
-                  image: NetworkImage(
-                    'https://avatars.githubusercontent.com/u/90053668?s=96&v=4',
-                  ),
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fill,
-                ),
-                // Text(
-                //   "Caros",
-                //   style: TextStyle(fontSize: 32, color: Colors.black87),
-                // ),
-                Image(
-                  image: NetworkImage(
-                    'https://avatars.githubusercontent.com/u/85492099?v=4',
-                  ),
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fill,
-                ),
-              ])),
-        ),
+        home: const MyHomePage(),
         theme: ThemeData(primaryColor: Colors.purple));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'โปรแกรมนับเลข',
+          style: TextStyle(fontSize: 30, color: Colors.lightGreenAccent),
+        ),
+        backgroundColor: Colors.black87,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text("กดปุ่มเพื่อเพิ่มจำนวนตัวเลข",
+              style: TextStyle(fontSize: 32)),
+          Text(number.toString(), style: const TextStyle(fontSize: 30)),
+          TextButton(
+            onPressed: addNumber,
+            style: TextButton.styleFrom(
+              fixedSize: const Size(210, 40),
+              foregroundColor: Colors.lightGreenAccent,
+              textStyle: const TextStyle(fontSize: 30),
+              backgroundColor: Colors.black87,
+            ),
+            child: const Text('TextButton'),
+          ),
+          ElevatedButton(
+            onPressed: removeNumber,
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size(210, 40),
+              foregroundColor: Colors.white54,
+              textStyle: const TextStyle(fontSize: 30),
+              backgroundColor: Colors.blueAccent,
+            ),
+            child: const Text('ElevatedButton'),
+          ),
+          OutlinedButton(
+            onPressed: clearNumber,
+            style: OutlinedButton.styleFrom(
+              fixedSize: const Size(210, 40),
+              foregroundColor: Colors.black87,
+              textStyle: const TextStyle(fontSize: 30),
+              side: const BorderSide(color: Colors.pink),
+            ),
+            child: const Text('OutlinedButton'),
+          )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNumber,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void addNumber() {
+    setState(() {
+      number++;
+    });
+  }
+
+  void clearNumber() {
+    setState(() {
+      number = 0;
+    });
+  }
+
+  void removeNumber(){
+    setState(() {
+      number--;
+    });
   }
 }
