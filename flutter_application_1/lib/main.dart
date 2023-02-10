9753ea07c2b46af90bd7b5cb784d18e34e1edc7d
Chollathit Saensaradee <huntgame2v2@gmail.com>
2023-02-10 03:00:20 -0800

    SS

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -32,45 +32,67 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   Color shadowColor = Colors.redAccent;
   bool isPressed = false;
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) => isLoading
+      ? const LoadingPage()
+      : Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        isLoading = true; //แสดง loading
+                      });
+                      await Future.delayed(Duration(seconds: 3)); //รอ3วิ
+                      setState(() {
+                        isLoading = false; //ซ่อน loading
+                      });
+                    },
+                    child: Text('Loading..')),
+                NeonButton(Colors.red),
+                NeonButton(Colors.greenAccent.shade700),
+                NeonButton(Colors.blue),
+                NeonButton(Colors.yellowAccent.shade700),
+                NeonButton(Colors.blueAccent.shade700),
+                NeonButton(Colors.white),
+                // SpinKitCircle(
+                //   size: 140,
+                //   itemBuilder: ((context, index) {
+                //     final colors = [Colors.green, Colors.red, Colors.yellow];
+                //     final color = colors[index % colors.length];
+                //     return DecoratedBox(
+                //         decoration:
+                //             BoxDecoration(color: color, shape: BoxShape.circle));
+                //   }),
+                // ),
+              ],
+            ),
+          ),
+        );
+}
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            NeonButton(Colors.red),
-            NeonButton(Colors.greenAccent.shade700),
-            NeonButton(Colors.blue),
-            NeonButton(Colors.yellowAccent.shade700),
-            NeonButton(Colors.blueAccent.shade700),
-            NeonButton(Colors.white),
-            SpinKitCircle(
-              size: 140,
-              itemBuilder: ((context, index) {
-                final colors = [Colors.green, Colors.red, Colors.yellow];
-                final color = colors[index % colors.length];
-                return DecoratedBox(
-                    decoration:
-                        BoxDecoration(color: color, shape: BoxShape.circle));
-              }),
-            ),
-          ],
-        ),
+    return Center(
+      child: SpinKitCircle(
+        size: 140,
+        itemBuilder: ((context, index) {
+          final colors = [Colors.green, Colors.red, Colors.yellow];
+          final color = colors[index % colors.length];
+          return DecoratedBox(
+              decoration: BoxDecoration(color: color, shape: BoxShape.circle));
+        }),
       ),
     );
   }
 }
-
-// class LoadingPage extends StatelessWidget {
-//   const LoadingPage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container();
-//   }
-// }
